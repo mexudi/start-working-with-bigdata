8794b6c3d857ebccb52c8a2c2b19febe497e9209
mexudi <soufianmexudi20@gmail.com>
2023-01-30 15:17:29 +0100

    first PySpark program

---
+++ task1.py
@@ -0,0 +1,10 @@
+from pyspark.sal import SparkSession
+
+spark = SparkSession \
+        .buidler \
+        .master("local[1]") \
+        .appName("myApp") \
+        .getOrCreate()
+
+df = spark.read.csv("./data.csv", header=True)
+df.show()
